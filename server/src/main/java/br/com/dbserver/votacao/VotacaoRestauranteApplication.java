5799875b5a6c0603b9cd21d705e423c6f47759fd
tevo33 <tevoguerra@gmail.com>
2025-03-26 21:27:48 -0300

    Add estrutura inicial

---
+++ server/src/main/java/br/com/dbserver/votacao/VotacaoRestauranteApplication.java
@@ -0,0 +1,11 @@
+package br.com.dbserver.votacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoRestauranteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VotacaoRestauranteApplication.class, args);
+    }
+} 
